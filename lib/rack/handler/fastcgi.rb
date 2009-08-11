c87b7cfdf0f978062acc7d63ebd0a1cd37a38f9d
dmann <darrinmann@gmail.com>
2009-08-11 13:56:50 -0700

    Checking that the FCGI::Stream class exists, before trying to override the read method.  The FCGI::Stream class does not exist in the pure ruby version of the FCGI module.  FCGI will use pure ruby if: 1. FCGI_PURE_RUBY = true 2. The C version of FCGI doesn't load (lib not found or the like)

--- lib/rack/handler/fastcgi.rb
+++ lib/rack/handler/fastcgi.rb
@@ -3,13 +3,15 @@ require 'socket'
 require 'rack/content_length'
 require 'rack/rewindable_input'
 
-class FCGI::Stream
-  alias _rack_read_without_buffer read
+if FCGI.const_defined? "Stream"
+  class FCGI::Stream
+    alias _rack_read_without_buffer read
 
-  def read(n, buffer=nil)
-    buf = _rack_read_without_buffer n
-    buffer.replace(buf.to_s)  if buffer
-    buf
+    def read(n, buffer=nil)
+      buf = _rack_read_without_buffer n
+      buffer.replace(buf.to_s)  if buffer
+      buf
+    end
   end
 end
 
